e376957a78f7843e266f808dda3578226552cd86
assassinscreed2 <ankursolanki1537@gmail.com>
2024-05-06 09:44:04 +0530

    Update App.js to render Header and Job Cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Grid } from "@mui/material";
+import "./App.css";
+
+import JobCard from "./components/JobCard";
+import Header from "./components/Header";
 
 function App() {
+  const [data, setData] = useState();
+  const [filteredData, setFilteredData] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      const body = JSON.stringify({
+        limit: 10,
+        offset: 0,
+      });
+
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body,
+      };
+
+      const response = await fetch(
+        "https://api.weekday.technology/adhoc/getSampleJdJSON",
+        requestOptions
+      );
+      const resData = await response.json();
+      console.log(resData);
+      setData(resData);
+      setFilteredData(resData);
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header data={data} setFilteredData={setFilteredData} />
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ marginTop: "3em" }}
+      >
+        {filteredData &&
+          filteredData.jdList.map((job) => {
+            return (
+              <Grid item container justifyContent="center" lg={4} md={6} sm={8}>
+                <JobCard job={job} />
+              </Grid>
+            );
+          })}
+      </Grid>
+    </>
   );
 }
 
